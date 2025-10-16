d0f9db78a9c6560514335357ad74eb95cb539cff
Ashish Shetty <ashish70300@gmail.com>
2025-10-16 12:21:43 +0000

    Added traffic generation

--- src/topo/mn_spineleaf_topo.py
+++ src/topo/mn_spineleaf_topo.py
@@ -1,29 +1,3 @@
-#!/usr/bin/python3
-
-"""
-MIT License
-
-Copyright (c) 2024 Maen Artimy
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import os
 import sys
 import yaml
@@ -35,6 +9,8 @@ from mininet.link import TCLink
 from mininet.log import setLogLevel
 from mininet.log import info
 from mininet.cli import CLI
+
+# from traffic_gen2 import generate_traffic
 
 class MyOVSSwitch(OVSSwitch):
     def __init__(self, *args, **kwargs):
@@ -89,6 +65,11 @@ def create_mininet_network(config_file, ctrl="127.0.0.1"):
     info("*** Starting switches\n")
     for switch in switches.values():
         switch.start([c0])
+
+    # info("*** Starting traffic flows\n")
+    # generate_traffic(net)   # run your custom traffic patterns
+
+    # info("*** Dropping to CLI\n")
 
     CLI(net)
 
--- src/topo/traffic_gen.py
+++ src/topo/traffic_gen.py
@@ -1,27 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2024 Maen Artimy
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from mininet.net import Mininet
 from mininet.log import setLogLevel, info
 import time
@@ -37,8 +13,9 @@ bandwidth = 100  # Kbps
 host_names = ["h1", "h2", "h3", "h4", "h5", "h6"]
 
 hosts = []
+net = Mininet()
 for h in host_names:
-    hosts.append(Mininet.get(h))
+    hosts.append(net.get(h))
 
 server = hosts[3]
 
